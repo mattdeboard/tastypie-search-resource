23826f9074b8215c71469d78d20722cc3fe7a46a
Matt DeBoard <matt.deboard@gmail.com>
2013-09-15 23:33:37 -0400

    Add setup.py so this is pip-installable.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='tastypie-search-resource',
+    version='1.0.0',
+    description='Tastypie search resource for django-haystack',
+    author='Matt DeBoard',
+    author_email='matt.deboard@gmail.com',
+    url='https://github.com/mattdeboard/tastypie-search-resource',
+    license='Other/Proprietary License',
+    packages=[
+        'tastypie_search_resource'
+    ]
+)
